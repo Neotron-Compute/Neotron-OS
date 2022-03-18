df45700b99affb43ea3147af22862d8efe698760
Jonathan Pallant (Ferrous Systems) <jonathan.pallant@ferrous-systems.com>
2022-03-18 17:56:09 +0000

    Formatting.

--- build.rs
+++ build.rs
@@ -3,20 +3,20 @@ fn main() {
     println!("cargo:rustc-link-arg-bin=flash0802=-Tneotron-flash-0802.ld");
     println!("cargo:rustc-link-arg-bin=flash0002=-Tneotron-flash-0002.ld");
 
-	if let Ok(cmd_output) = std::process::Command::new("git")
-		.arg("describe")
-		.arg("--all")
-		.arg("--dirty")
-		.arg("--long")
-		.output()
-	{
-		let git_version = std::str::from_utf8(&cmd_output.stdout).unwrap();
-		println!(
-			"cargo:rustc-env=OS_VERSION={} (git:{})",
-			env!("CARGO_PKG_VERSION"),
-			git_version.trim()
-		);
-	} else {
-		println!("cargo:rustc-env=OS_VERSION={}", env!("CARGO_PKG_VERSION"));
-	}
+    if let Ok(cmd_output) = std::process::Command::new("git")
+        .arg("describe")
+        .arg("--all")
+        .arg("--dirty")
+        .arg("--long")
+        .output()
+    {
+        let git_version = std::str::from_utf8(&cmd_output.stdout).unwrap();
+        println!(
+            "cargo:rustc-env=OS_VERSION={} (git:{})",
+            env!("CARGO_PKG_VERSION"),
+            git_version.trim()
+        );
+    } else {
+        println!("cargo:rustc-env=OS_VERSION={}", env!("CARGO_PKG_VERSION"));
+    }
 }
