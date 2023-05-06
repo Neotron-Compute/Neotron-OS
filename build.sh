0a5aa289b20117d7639097d46a6b54a051927b9c
Jonathan Pallant (Ferrous Systems) <jonathan.pallant@ferrous-systems.com>
2023-05-06 21:41:32 +0100

    Updated to latest experimental embedded-sdmmc.

--- build.sh
+++ build.sh
@@ -11,9 +11,9 @@ for TARGET_ARCH in thumbv6m-none-eabi thumbv7m-none-eabi thumbv7em-none-eabi; do
   echo "TARGET is ${TARGET_ARCH}"
   for BINARY in flash0002 flash0802 flash1002; do
     echo "BINARY is ${BINARY}"
-    cargo build --verbose --release --target=${TARGET_ARCH} --bin ${BINARY} --features=defmt-log
+    cargo build --verbose --release --target=${TARGET_ARCH} --bin ${BINARY}
     # objcopy would do the build for us first, but it doesn't have good build output
-    cargo objcopy --verbose --release --target=${TARGET_ARCH} --bin ${BINARY} --features=defmt-log -- -O binary ${RELEASE_DIR}/${TARGET_ARCH}-${BINARY}-libneotron_os.bin
+    cargo objcopy --verbose --release --target=${TARGET_ARCH} --bin ${BINARY} -- -O binary ${RELEASE_DIR}/${TARGET_ARCH}-${BINARY}-libneotron_os.bin
     # Keep the ELF file too (for debugging)
     cp ./target/${TARGET_ARCH}/release/${BINARY} ${RELEASE_DIR}/${TARGET_ARCH}-${BINARY}-libneotron_os.elf
   done
@@ -21,5 +21,5 @@ done
 
 # Build the host version
 echo "Building HOST"
-cargo build --verbose --lib --release --target=x86_64-unknown-linux-gnu --features=native-log
+cargo build --verbose --lib --release --target=x86_64-unknown-linux-gnu
 cp ./target/x86_64-unknown-linux-gnu/release/libneotron_os.so ${RELEASE_DIR}/x86_64-unknown-linux-gnu-libneotron_os.so
