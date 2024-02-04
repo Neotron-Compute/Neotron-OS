4e5bcea5fce13045261c1fbad978874a7773464c
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2024-02-04 17:57:05 +0000

    Make paths non-relative in build.sh
    Now you can call the script from outside the tree.

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 set -euo pipefail
 
-RELEASE_DIR=./release
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+RELEASE_DIR=${SCRIPT_DIR}/release
 
 mkdir -p ${RELEASE_DIR}
 
@@ -11,15 +13,15 @@ for TARGET_ARCH in thumbv6m-none-eabi thumbv7m-none-eabi thumbv7em-none-eabi; do
   echo "TARGET is ${TARGET_ARCH}"
   for BINARY in flash0002 flash0802 flash1002; do
     echo "BINARY is ${BINARY}"
-    cargo build $* --release --target=${TARGET_ARCH} --bin ${BINARY}
+    ( cd ${SCRIPT_DIR} && cargo build $* --release --target=${TARGET_ARCH} --bin ${BINARY} )
     # objcopy would do the build for us first, but it doesn't have good build output
-    rust-objcopy -O binary ./target/${TARGET_ARCH}/release/${BINARY} ${RELEASE_DIR}/${TARGET_ARCH}-${BINARY}-libneotron_os.bin
+    rust-objcopy -O binary ${SCRIPT_DIR}/target/${TARGET_ARCH}/release/${BINARY} ${RELEASE_DIR}/${TARGET_ARCH}-${BINARY}-libneotron_os.bin
     # Keep the ELF file too (for debugging)
-    cp ./target/${TARGET_ARCH}/release/${BINARY} ${RELEASE_DIR}/${TARGET_ARCH}-${BINARY}-libneotron_os.elf
+    cp ${SCRIPT_DIR}/target/${TARGET_ARCH}/release/${BINARY} ${RELEASE_DIR}/${TARGET_ARCH}-${BINARY}-libneotron_os.elf
   done
 done
 
 # Build the host version
 echo "Building HOST"
-cargo build --verbose --lib --release --target=x86_64-unknown-linux-gnu
-cp ./target/x86_64-unknown-linux-gnu/release/libneotron_os.so ${RELEASE_DIR}/x86_64-unknown-linux-gnu-libneotron_os.so
+( cd ${SCRIPT_DIR} && cargo build --verbose --lib --release --target=x86_64-unknown-linux-gnu )
+cp ${SCRIPT_DIR}/target/x86_64-unknown-linux-gnu/release/libneotron_os.so ${RELEASE_DIR}/x86_64-unknown-linux-gnu-libneotron_os.so
